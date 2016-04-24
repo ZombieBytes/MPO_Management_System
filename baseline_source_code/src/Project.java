69810dba33da4127f77ecd1f66f478dcd6918484
Christian <ourtwoism@gmail.com>
2016-04-23 23:38:59 -0600

    added some basic attributes/variables into the Project.java file

--- baseline_source_code/src/Project.java
+++ baseline_source_code/src/Project.java
@@ -11,4 +11,38 @@ public class Project {
 	 * Responsibilities:
 	 *  -Knows project information
 	 */
+	String airQualityAnalysis_CO;
+	String airQualityAnalysis_NOX;
+	String airQualityAnalysis_VOC;
+	String area;
+	boolean capacityProject;
+	String city;
+	String[] county = {"El Paso", "Dona Ana"};
+	String[] DOT_District = {"Texas District 24", "NM District 1",
+							"NM District 2"};
+	int federalFiscalYear;
+	String federalFunctionalClassification;
+	String[] fundProjectType = {"Highway/Roadway", "Transit"};
+	String highwayRoadwayName;
+	
+	String limitFrom;
+	String limitTo;
+	String networkYear;
+	int numberOfExistingLanes;
+	int numberOfMiles;
+	int numberOfProjectedLanes;
+	String projectDescription;
+	String project_ID;
+	String projectName;
+	String projectType;
+	
+	//Project phases(don't know type)
+	String remarks;
+	String scopeOfWork;
+	String sponsorAgency;
+	String[] stateSystemRoad = {"ON_State System Road",
+						"OFF_State System Road", "ON_OFF State System"
+								+ "Road"};
+	
+	String transitInformation;
 }
